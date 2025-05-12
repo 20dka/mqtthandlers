44d17f4a14e4c2d649a977218b51a5dddd9cc0f6
20dka <carp@bleat.hu>
2025-05-12 19:08:27 +0000

    add timer extend logic to motion_bathroom

--- handlers/motion_bathroom.lua
+++ handlers/motion_bathroom.lua
@@ -6,24 +6,25 @@ local function turn_off()
 	client:publish{ topic="zigbee2mqtt/bulb_bathroom/set", payload=json.encode{state = "OFF"}}
 end
 
+local BRIGHTNESS_DIM = 2
+
 return {
 	topic = "zigbee2mqtt/motion_bathroom",
 	pattern = "zigbee2mqtt/motion_bathroom",
 	on_match = function(payload)
 		local occupied = json.decode(payload).occupancy
 		if occupied then
-			turn_on(2)
-			log('motion_bathroom', 'turning light on (dim) (timed)')
+			if not events.get("bathroom_timer") then
+				turn_on(BRIGHTNESS_DIM)
+				log('motion_bathroom', 'turning light on (dim) (timed)')
 
-			events.add('bathroom_timer', 60*20, nil, function()
-				turn_off()
-				log('motion_bathroom', 'turning light off (timer ran out)')
-			end)
-		else
-			turn_off()
-			log('motion_bathroom', 'turning light off (no longer occupied)')
-
-			events.remove('bathroom_timer')
+				events.add('bathroom_timer', 60*20, nil, function()
+					turn_off()
+					log('motion_bathroom', 'turning light off (timer ran out)')
+				end)
+			elseif events.extend("bathroom_timer", 60*20) then
+				log('motion_bathroom', 'extending light')
+			end
 		end
 	end
 }
