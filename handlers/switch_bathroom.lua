ad290a06a4696ca29d34095fe33e100d93f0a295
20dka <carp@bleat.hu>
2025-05-12 19:05:59 +0000

    tidy up switch_bathroom

--- handlers/switch_bathroom.lua
+++ handlers/switch_bathroom.lua
@@ -6,24 +6,30 @@ local function turn_off()
 	client:publish{ topic="zigbee2mqtt/bulb_bathroom/set", payload=json.encode{state = "OFF"}}
 end
 
+local function timer_ran_out()
+	turn_off()
+	log('switch_bathroom', 'turning light off (timer ran out)')
+end
+
+local BRIGHTNESS_DIM, BRIGHTNESS_FULL = 2, 254
+
 return {
 	topic = "zigbee2mqtt/switch_bathroom",
 	pattern = "zigbee2mqtt/switch_bathroom",
 	on_match = function(payload)
 		local action = json.decode(payload).action
-		if action == 'on' then
-			turn_on(2)
+		if action == 'on' then -- short press
+			turn_on(BRIGHTNESS_DIM)
+
 			log('switch_bathroom', 'turning light on (dim) (timed)')
 
-			events.add('bathroom_timer', 60*60*1, nil, function()
-				turn_off()
-				log('switch_bathroom', 'turning light off (timer ran out)')
-			end)
+			events.add('bathroom_timer', 60*30, nil, timer_ran_out)
 		elseif action == 'brightness_move_up' then -- long press
-			turn_on(254)
+			turn_on(BRIGHTNESS_FULL)
+
 			log('switch_bathroom', 'turning light on (bright)')
 
-			events.remove('bathroom_timer')
+			events.add('bathroom_timer', 60*30, nil, timer_ran_out)
 		elseif action == 'off' then -- short press
 			turn_off()
 			log('switch_bathroom', 'turning light off (manual)')
