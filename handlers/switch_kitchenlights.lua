45c39bb2c8cde5167d4e68b4c575a6f4f39656aa
20dka <carp@bleat.hu>
2026-01-18 00:24:07 +0000

    script: kitchen wled dimming

--- handlers/switch_kitchenlights.lua
+++ handlers/switch_kitchenlights.lua
@@ -1,4 +1,6 @@
+local socket = nil
 local strip_timer = 'kitchen_strip_timer'
+local strip_dim_timer = 'kitchen_strip_brightness_timer'
 
 local function turn_on(brightness)
 	client:publish{ topic="wled/kitchen", payload=(brightness and tostring(brightness) or "ON") }
@@ -13,17 +15,41 @@ local function timer_ran_out()
 	log('switch_kitchen', 'turning light strip off (timer ran out)')
 end
 
+local function timer_dim()
+	local now = socket.gettime()
+	local delta = now - dim_last
+	if delta > 0.5 then
+		client:publish{ topic="wled/kitchen/api", payload=string.format('{"bri":"~%s30"}', dim_direction and '-' or '') }
+		dim_last = now
+	end
+end
+
 return {
+	on_init = function()
+		socket = require('socket')
+	end,
 	topic = "zigbee2mqtt/kitchen/switch/negygombos",
 	pattern = "zigbee2mqtt/kitchen/switch/negygombos",
 	on_match = function(payload)
-		local action = json.decode(payload).action
-		if action == 'arrow_left_click' or action == 'arrow_right_click' then -- short press (either side)
+		local dir, action = string.match(json.decode(payload).action, "arrow_(%a+)_(%a+)")
+
+		if action == 'click' then -- short press (either side)
 			turn_on('T') -- toggle
 
 			log('switch_kitchen', 'toggling strip (timed)')
 
 			events.add(strip_timer, 60*60*4, nil, timer_ran_out)
+		elseif action == 'hold' then
+			dim_last = socket.gettime()
+			dim_direction = dir == 'left'
+			events.add(strip_dim_timer, 20, timer_dim, nil)
+
+			log('switch_kitchen', 'beginning kitchen lights dimming, going', dim_direction and 'down' or 'up')
+
+		elseif action == 'release' then
+			events.remove(strip_dim_timer)
+
+			log('switch_kitchen', 'removing dimming timer')
 		end
 	end
 }
