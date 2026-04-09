c7c794d7f5fb2e0a74d4d7ed4bda8c3069beb26d
20dka <carp@bleat.hu>
2026-04-09 23:08:58 +0000

    script: clean up mqttdroid bridge

--- handlers/mqttdroid_desklamp.lua
+++ handlers/mqttdroid_desklamp.lua
@@ -6,13 +6,13 @@ local patterns = { "(zigbee2mqtt)/livingroom/outlet/desk_lamp", "(mqttdroid)/des
 return {
 	topic = { zigbeepath, droidpath .."/set" },
 	pattern = patterns,
-	on_match = function(payload, name)
-		if name == "mqttdroid" then
-			log("mqttdroid", "telling the light to turn", payload)
+	on_match = function(payload, source)
+		if source == "mqttdroid" then
+			log("mqttdroid", "telling the desklamp to turn", payload)
 			client:publish{ topic=zigbeepath .. "/set", payload = json.encode{state = payload} }
-		elseif name == "zigbee2mqtt" then
-			log("mqttdroid", "light is telling us it turned", json.decode(payload).state)
-			client:publish{ topic=droidpath, payload = json.decode(payload).state }
+		elseif source == "zigbee2mqtt" then
+			log("mqttdroid", "desklamp is telling us it turned", json.decode(payload).state)
+			client:publish{ topic=droidpath, payload = json.decode(payload).state, retain=true }
 		end
 	end
 }
